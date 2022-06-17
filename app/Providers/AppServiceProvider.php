93abce9eb88fbf4c302a8d8ea013b35ffd50ca9c
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-06-17 23:23:47 +0700

    add timezone at boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        date_default_timezone_set('Asia/Jakarta');
         if (! $this->app->routesAreCached()) {
             
         }
