6d73e1ffd502003deaef7e586ef42ff4edd93ddd
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-03-01 14:17:34 +0700

    add pivot in worktime workunit

--- app/Models/Workunit.php
+++ app/Models/Workunit.php
@@ -41,7 +41,7 @@ class Workunit extends Model
     
     function worktimes()
     {
-        return $this->belongsToMany(Worktime::class)->withPivot('date_start','date_end');;
+        return $this->belongsToMany(Worktime::class,'workunit_worktime')->withPivot('date_start','date_end');;
     }
 
     function place()
