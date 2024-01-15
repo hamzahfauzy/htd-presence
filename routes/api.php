78682a63a796a59053ccd6436874055d17b510eb
CyberPanel <support@cyberpanel.net>
2024-01-15 22:21:59 +0700

    update api middleware

--- routes/api.php
+++ routes/api.php
@@ -47,7 +47,7 @@ Route::prefix('dashboards')->group(function () {
     Route::get('index', [DashboardApiController::class, 'index']);
 });
 
-Route::middleware('auth:sanctum', 'versionChecker')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('auth/change-password', [AuthController::class, 'changePassword']);
     Route::post('auth/change-email', [AuthController::class, 'changeEmail']);
